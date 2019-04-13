68b754fe924b8a4e2d36c02536c9217760e3a624
Steven Perrin <spcnn@mail.missouri.edu>
2019-04-12 19:10:48 -0500

    Created Converter Struct

---
+++ Conversion-Calculator/Conversion-Calculator/ConverterStruct.swift
@@ -0,0 +1,15 @@
+//
+//  ConverterStruct.swift
+//  Conversion-Calculator
+//
+//  Created by Steven Perrin on 4/12/19.
+//  Copyright © 2019 Steven Perrin. All rights reserved.
+//
+
+import UIKit
+
+struct Converter {
+    let label: String
+    let inputUnit: String
+    let outputUnit: String
+}
--- Conversion-Calculator/Conversion-Calculator/ConverterViewController.swift
+++ Conversion-Calculator/Conversion-Calculator/ConverterViewController.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2019 Steven Perrin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CalculatorConverterViewController: UIViewController {
+    
+    
+    var converter = [Converter(label: "fahrenheit to celcius", inputUnit: "°F", outputUnit: "°C"),
+                     Converter(label: "celcius to fahrenheit", inputUnit: "°C", outputUnit: "°F"),
+                     Converter(label: "miles to kilometers", inputUnit: "mi", outputUnit: "km"),
+                     Converter(label: "kilometers to miles", inputUnit: "km", outputUnit: "mi")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}
